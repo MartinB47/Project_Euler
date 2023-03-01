996511f1d9c91e8cff0ed5c1d48138e0584b29f4
Martin <101028844+MartinB47@users.noreply.github.com>
2023-02-28 19:37:07 -0800

    Add files via upload

---
+++ Problem_042-Coded_Triangle_Numbers.cpp
@@ -0,0 +1,31 @@
+/*
+ The nth term of the sequence of triangle numbers is given by, tn = ½n(n+1); so the first ten triangle numbers are:
+
+ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+
+ By converting each letter in a word to a number corresponding to its alphabetical position and adding these values we form a word value. For example, the word value for SKY is 19 + 11 + 25 = 55 = t10. If the word value is a triangle number then we shall call the word a triangle word.
+
+ Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words, how many are triangle words?
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ifstream file("words.txt");
+    string word;
+    int count = 0;
+    while (getline(file, word, ',')){
+        word.erase(0, 1);
+        word.erase(word.size() - 1);
+        int sum = 0;
+        for (char ch: word){
+            sum += ch - 'A' + 1;
+        }
+        if (isTriangleNumber(sum)){
+            count++;
+        }
+    }
+    file.close();
+    cout << count << endl;
+}
