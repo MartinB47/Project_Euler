fa8d260cfea6a8aace157aa63cdd5cf7788e2c5a
Martin <101028844+MartinB47@users.noreply.github.com>
2023-07-05 07:30:56 -0700

    Add files via upload

--- Problem_070-Totient_Permutation.cpp
+++ Problem_070-Totient_Permutation.cpp
@@ -1,3 +1,13 @@
+/*
+ Euler’s Totient function, Φ(n) [sometimes called the phi function], is used to determine the number of positive numbers less than or equal to n which are relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, Φ(9)=6.
+
+ The number 1 is considered to be relatively prime to every positive number, so Φ(1)=1.
+
+ Interestingly, Φ(87109)=79180, and it can be seen that 87109 is a permutation of 79180.
+
+ Find the value of n, 1 < n < 10^7, for which Φ(n) is a permutation of n and the ratio n/Φ(n) produces a minimum.
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
