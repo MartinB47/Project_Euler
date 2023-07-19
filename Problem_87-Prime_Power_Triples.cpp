073422e3a3a000bdb5acb4619e07de9f274eb91f
Martin <101028844+MartinB47@users.noreply.github.com>
2023-07-18 22:35:16 -0700

    Add files via upload

--- Problem_87-Prime_Power_Triples.cpp
+++ Problem_87-Prime_Power_Triples.cpp
@@ -1,73 +1,64 @@
 /*
- The smallest number expressible as the sum of a prime square, prime cube, and prime fourth power is 28. In fact, there are exactly four numbers below fifty that can be expressed in such a way:
-
- 28 = 2^2 + 2^3 + 2^4
- 33 = 3^2 + 2^3 + 2^4
- 49 = 5^2 + 2^3 + 2^4
- 47 = 2^2 + 3^3 + 2^4
-
+ The smallest number expressible as the sum of a prime square, prime cube, and prime fourth power is
+ . In fact, there are exactly four numbers below fifty that can be expressed in such a way:
+         28 = 2^2 + 2^3 + 2^4
+         33 = 3^2 + 2^3 + 2^4
+         49 = 5^2 + 2^3 + 2^4
+         47 = 2^2 + 3^3 + 2^4
  How many numbers below fifty million can be expressed as the sum of a prime square, prime cube, and prime fourth power?
- */
+*/
 
-#include <iostream>
-#include <cmath>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-bool prime(int num) {
-    if (num == 1) return false;
-    for (int i = 2; i <= int(num / 2); i++) if (num % i == 0) return false;
-    return true;
-}
-
-
-int duplicates(int array[], int size){
-    int count = 0;
-    for(int i=0; i<size; i++){
-       for(int j=i+1; j<size; j++){
-           if(array[i] == array[j]){
-                   count++;
-                   break;
+vector<int> sieve(int n) {
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    
+    // Implementing the Sieve of Eratosthenes algorithm
+    for (int i = 2; i * i <= n; ++i) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                is_prime[j] = false;
             }
         }
     }
-    return count;
+    
+    // Collecting the primes into a separate vector
+    vector<int> primes;
+    for (int i = 0; i <= n; ++i) {
+        if (is_prime[i]) {
+            primes.push_back(i);
+        }
+    }
+    
+    return primes;
 }
-int * prime_power_triple(int num){
-    int index = 0;
-    static int power_triples[1139575];
-    for (int square = 2; square <= 7072; square++){
-        int result = 0;
-        if (prime(square)){
-            for (int cube = 2; cube <= 369; cube++){
-                if (prime(cube)){
-                    for (int fourth = 2; fourth <= 85; fourth++){
-                        if (prime(fourth)){
-                            result = pow(square,2) + pow(cube,3) + pow(fourth,4);
-                            if (result <= num){
-                                power_triples[index++] = result;
-                                //cout << result << endl;
-                                //cout << " = " << square << " " << cube << " " << fourth << endl;
-                            }
-                        }
-                    }
-                }
+
+int main() {
+    const int limit = 5e7;
+    // Calculating the limit for the sieve function
+    const int sieve_limit = sqrt(limit);
+    vector<int> primes = sieve(sieve_limit);
+    
+    unordered_set<int> unique_sums;
+    for (const int& p1 : primes) {
+        int sum1 = pow(p1, 4);
+        if (sum1 >= limit) break;
+
+        for (const int& p2 : primes) {
+            int sum2 = sum1 + pow(p2, 3);
+            if (sum2 >= limit) break;
+
+            for (const int& p3 : primes) {
+                int sum3 = sum2 + pow(p3, 2);
+                if (sum3 >= limit) break;
+
+                unique_sums.insert(sum3);
             }
         }
     }
-    return power_triples;
-}
     
-int main(){
-    int *numbers;
-    int size = 1139575;
-    numbers = prime_power_triple(50000000);
-    sort(numbers, numbers + 1139575);
-    
-    for (int i = 0; i<size; i++){
-        cout << numbers[i] << endl;
-    }
-    
-    //cout << "Duplicates: " << duplicates(numbers, 1139575);
+    // Output the size of the set (i.e., the count of unique sums)
+    cout << unique_sums.size();
 }
